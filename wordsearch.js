6183c23d0ae1bac49ddebb83a34976e5f8763348
Ife Olaifa <ifeolaifa5@gmail.com>
2020-09-24 01:14:25 +0000

    worked with @charlesthompson

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,28 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    let emptyArray = []; 
+    let message = "";
+
     for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+        if (l.includes(word)) 
+        return true;
     }
-}
+
+    for(let i=0; i < letters[0].length; i++) {
+        message = "";
+         for(let j=0; j < letters[i].length; j++) {
+            message += letters[j][i]; 
+         }
+         emptyArray.push(message);
+     }
+
+      if(emptyArray.includes(word)) {
+        return true; 
+      }
+
+      return false; 
+
+  }
+
 
 module.exports = wordSearch
